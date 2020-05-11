f595d50bfd61369c193f3ad6a6259f60df763b0f
Mohammed Mokhles <moha199612@gmail.com>
2020-05-11 05:34:47 +0200

    react-redux-firebase setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+
+import store from "./store";
+import { rrfProps } from "./store";
+
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 
@@ -8,16 +14,20 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppNavbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <Router>
+          <div className="App">
+            <AppNavbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,52 @@
+import { createStore, combineReducers, compose } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { firebaseReducer } from "react-redux-firebase";
+import { createFirestoreInstance, firestoreReducer } from "redux-firestore";
+
+// todo: Reducers
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3x6W7UBrNK7MgMgdnrK6NXtlRs-9mO-c",
+  authDomain: "reactclientpanel-cf073.firebaseapp.com",
+  databaseURL: "https://reactclientpanel-cf073.firebaseio.com",
+  projectId: "reactclientpanel-cf073",
+  storageBucket: "reactclientpanel-cf073.appspot.com",
+  messagingSenderId: "137856077852",
+  appId: "1:137856077852:web:616c60f5222ee7413f8fd9",
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+};
+
+//init firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// init firestore
+// const firestore = firebase.firestore();
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
+});
+
+// Create init state
+const initialState = {};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Create Store
+const store = createStore(rootReducer, initialState, composeEnhancers());
+
+export const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, // <- needed if using firestore
+};
+
+export default store;
